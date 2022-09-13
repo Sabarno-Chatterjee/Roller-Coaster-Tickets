bf5cb4dd6785d3e1b2357865246748479f3d229b
Sabarno Chatterjee <106575210+Sabarno-Chatterjee@users.noreply.github.com>
2022-09-13 12:39:34 +0530

    Add files via upload

--- Roller_coaster.py
+++ Roller_coaster.py
@@ -3,11 +3,20 @@ height = int(input("What is your height in cm? "))
 if height >= 120:
     print("You can ride the roller coaster")
     age = int(input("Please enter your age"))
+    bill = 0
     if age < 12:
-        print("You have to pay $5.")
+        bill = 5
+        print("Child tickets are $5.")
     elif age < 18:
-        print("You have to pay $7")
+        bill = 7
+        print("Youth tickets are $7")
     else:
-        print("You have to pay $15. ")
+        bill = 15
+        print("Adult tickets are $15. ")
+    pic = input("Do you want a photo taken? Enter Y for Yes and N for No.")
+    if pic == "Y":
+        bill += 3
+        print(f"Your final bill is {bill}")
+    print(f"Your final bill is {bill}")
 else:
     print("You cannot ride the roller coaster")
